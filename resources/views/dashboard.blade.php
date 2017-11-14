f6a15c8de144c1682dbd567e7c929c2263094c5f
Richard Parayno <broodheart@gmail.com>
2017-11-14 23:46:17 +0800

    converted design to laravel and updated nav links

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,25 +5,72 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Carbon Emission Dashboard</title>
-  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
   <link rel="stylesheet" type="text/css" href="{{ url('/css/normalize.css') }}">
   <link rel="stylesheet" type="text/css" href="{{ url('/css/skeleton.css') }}">
   <link rel="stylesheet" type="text/css" href="{{ url('/css/style-dash.css') }}">
+  <style>
+    #trips-overview-div {
+      margin-top: 30px;
+      background: #363635;
+      box-shadow: 5px 10px 20px 0 rgba(0,0,0,0.20);  
+    }
+  </style>
 </head>
 <body>
   <!-- side nav -->
   <div class="container u-pull-left" id="sidebar">
     <div class="twelve column bar">
-      <img src="{{ url('/images/favicon.png') }}" alt="Icon"><a href="#">Richard Parayno</a>
+      <strong><p style="text-align: center; margin: 0px;">Carbon Emission Dashboard</p></strong>
+    </div>
+
+    <div class="twelve column bar">
+      <span><strong>Current User:</strong></span>
+      <p>Systems Administrator</p>
     </div>
     <div class="twelve column bar">
-      <img src="{{ url('/images/favicon.png') }}" alt="Icon"><a href="#">Emissions</a>
+      <span><strong>Home</strong></span>
+      <ul>
+        <li>Dashboard</li>
+        <ul>
+          <li><a href="{{ route('dashboard') }}">View Dashboard</a></li>
+        </ul>
+      </ul>
     </div>
     <div class="twelve column bar">
-      <img src="{{ url('/images/favicon.png') }}" alt="Icon"><a href="#">Trips</a>
+      <span><strong>Account Management</strong></span>
+      <ul>
+        <li>User Accounts</li>
+        <ul>
+          <li><a href="{{ route('user-view') }}">View Users</a></li>
+          <li><a href="{{ route('user-add') }}">Create New User Account</a></li>
+          <li><a href="{{ route('user-editinfo') }}">Edit User Account Information</a></li>
+          <li><a href="{{ route('user-editcreds') }}">Edit User Account Credentials</a></li>
+        </ul>
+      </ul>
     </div>
     <div class="twelve column bar">
-      <img src="{{ url('/images/favicon.png') }}" alt="Icon"><a href="#">Analytics</a>
+      <span><strong>Campus Information Management</strong></span>
+      <ul>
+        <li>Campuses/Institutes</li>
+        <ul>
+          <li><a href="{{ route('campus-view') }}">View Campuses/Institutes</a></li>
+          <li><a href="{{ route('campus-add') }}">Add New Campus/Institute</a></li>
+          <li><a href="{{ route('campus-editinfo') }}">Edit Campus/Institute Info</a></li>
+        </ul>
+        <li>Department/Offices</li>
+        <ul>
+          <li><a href="{{ route('department-view') }}">View Departments/Offices</a></li>
+          <li><a href="{{ route('department-add') }}">Add New Department/Offices</a></li>
+          <li><a href="{{ route('department-editinfo') }}">Edit Department/Offices Info</a></li>          
+        </ul>
+        <li>Vehicles</li>
+        <ul>
+          <li><a href="{{ route('vehicle-view') }}">View Vehicles</a></li>
+          <li><a href="{{ route('vehicle-add') }}">Add New Vehicle</a></li>
+          <li><a href="{{ route('vehicle-editinfo') }}">Edit Vehicle Info</a></li>
+          <li><a href="{{ route('vehicle-decommission') }}">Decommission Vehicle</a></li>
+        </ul>
+      </ul>
     </div>
   </div>
   <!-- side nav -->
--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,74 @@ Route::get('/', function () {
 /* Dashboard Route */
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
+})->name('dashboard');
 
 /* Submit Form Route */
 Route::get('/submit', function () {
     return view('submit');
 });
+
+/* User Account Management Routes */
+/* View Users */
+Route::get('/dashboard/user-view', function() {
+    return view('user-view');
+})->name('user-view');
+/* Create New User Account */
+Route::get('/dashboard/user-add', function() {
+    return view('user-add');
+})->name('user-add');
+/* Edit User Account Information */
+Route::get('/dashboard/user-editinfo', function() {
+    return view('user-editinfo');
+})->name('user-editinfo');
+/* Edit User Account Credentials */
+Route::get('/dashboard/user-editcreds', function() {
+    return view('user-editcreds');
+})->name('user-editcreds');
+
+/* Campus Information Management Routes */
+/* View Campus */
+Route::get('/dashboard/campus-view', function() {
+    return view('campus-view');
+})->name('campus-view');
+/* Add New Campus/Institute */
+Route::get('/dashboard/campus-add', function() {
+    return view('campus-add');
+})->name('campus-add');
+/* Edit Campus/Institute */
+Route::get('/dashboard/campus-editinfo', function() {
+    return view('campus-editinfo');
+})->name('campus-editinfo');
+
+/* View Departments/Offices */
+Route::get('/dashboard/department-view', function() {
+    return view('department-view');
+})->name('department-view');
+/* Add New Department/Offices */
+Route::get('/dashboard/department-add', function() {
+    return view('department-add');
+})->name('department-add');
+/* Edit Department/Offices */
+Route::get('/dashboard/department-editinfo', function() {
+    return view('department-editinfo');
+})->name('department-editinfo');
+
+/* View Vehicles */
+Route::get('/dashboard/vehicle-view', function() {
+    return view('vehicle-view');
+})->name('vehicle-view');
+/* Add New Vehicle */
+Route::get('/dashboard/vehicle-add', function() {
+    return view('vehicle-add');
+})->name('vehicle-add');
+/* Edit Vehicle */
+Route::get('/dashboard/vehicle-editinfo', function() {
+    return view('vehicle-editinfo');
+})->name('vehicle-editinfo');
+/* Decommission Vehicle */
+Route::get('/dashboard/vehicle-decommission', function() {
+    return view('vehicle-decommission');
+})->name('vehicle-decommission');
 
 Auth::routes();
 
