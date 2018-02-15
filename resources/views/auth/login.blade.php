9de2a98387a178bce70ef01611163a703136a807
Richard Parayno <broodheart@gmail.com>
2018-02-15 17:52:03 +0800

    added web font to login screen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Carbon Emission Dashboard</title>
-  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="{{ url('/css/normalize.css') }}">
   <link rel="stylesheet" type="text/css" href="{{ url('/css/skeleton.css') }}">
   <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}">
