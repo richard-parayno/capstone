a262cfa46214531e47804e13101a7dbe2132ab0c
Richard Parayno <broodheart@gmail.com>
2018-02-23 21:42:56 +0800

    began work on edit user info

--- resources/views/user-editinfo.blade.php
+++ resources/views/user-editinfo.blade.php
@@ -24,6 +24,16 @@
   <!-- TODO: Process edit-user logic after submitting form. -->
   <h1>Edit User Account Information</h1>
   <form action="/edit-user">
+    <div class="twelve columns" style="margin: 0px;">
+        <p>Selected User: </p>
+        @php
+          $currentUser = $_GET['user'];
+          $currentUserName = $_GET['name'];
+          echo $currentUserName;
+          echo ("<input class=\"u-full-width\" type=\"hidden\" name=\"user\" id=\"user\" value=\"$currentUser\">");
+        @endphp
+        
+      </div>
     <div class="six columns" style="margin: 0px;">
       <label for="first-name">First Name</label>
       <input class="u-full-width" type="text" name="first-name" id="first-name" placeholder="Richard Lance">
