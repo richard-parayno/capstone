20d30b02e5872b9ea94914a40a35c3672d11d237
Richard Parayno <broodheart@gmail.com>
2018-03-01 21:44:41 +0800

    fixing the view pa. incomplete logic for the excel controller but it works

--- resources/views/display-table.blade.php
+++ resources/views/display-table.blade.php
@@ -25,7 +25,7 @@
 
 
 <div class="seven column" id="box-form">
-  <h1>View Uploaded Excel File</h1>
+  <h1>Excel Data</h1>
   <table>
     <thead>
       <tr>
@@ -39,30 +39,64 @@
     <tbody>
       @php
       $y = count($data);
+      $throw = array();
       @endphp
       
 
       @for($x = 0; $x < $y; $x++)
         @php 
-          $checker = DB::table('vehicles_mv')->where('plateNumber', $data[$x]['plate_number'])->first();
-          if ($checker == null) {
-          
-          } else {
-            echo "<tr>";
-            echo "<td>".$data[$x]['date']."</td>";
-            echo "<td>".$data[$x]['requesting_department']."</td>";
-            echo "<td>".$data[$x]['plate_number']."</td>";
-            echo "<td>".$data[$x]['kilometer_reading']."</td>";
-            echo "<td>".$data[$x]['destinations']."</td>";
-            echo "</tr>";
-          }
+        $checker = DB::table('vehicles_mv')->where('plateNumber', $data[$x]['plate_number'])->first();
+        if ($checker == null) {
+          $throw[$x]['date'] = $data[$x]['date'];
+          $throw[$x]['requesting_department'] = $data[$x]['requesting_department'];
+          $throw[$x]['plate_number'] = $data[$x]['plate_number'];
+          $throw[$x]['kilometer_reading'] = $data[$x]['kilometer_reading'];
+          $throw[$x]['destinations'] = $data[$x]['destinations'];
+        } else {
+          echo "<tr>";
+          echo "<td>".$data[$x]['date']."</td>";
+          echo "<td>".$data[$x]['requesting_department']."</td>";
+          echo "<td>".$data[$x]['plate_number']."</td>";
+          echo "<td>".$data[$x]['kilometer_reading']."</td>";
+          echo "<td>".$data[$x]['destinations']."</td>";
+          echo "</tr>";
+        }
         @endphp
       @endfor
 
+      
     </tbody>
 
 
   </table>
+
+  <h1>Dataset Errors</h1>
+  @php
+  if ($throw != null) {
+    echo "<table>";
+    echo "<thead>";
+    echo  "<tr>";
+    echo    "<th>Date</th>";
+    echo    "<th>Requesting Department</th>";
+    echo    "<th>Plate Number</th>";
+    echo    "<th>Kilometer Reading</th>";
+    echo    "<th>Destinations</th>";
+    echo  "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+    for($x = 0; $x < count($throw); $x++) {
+      echo"<tr>";
+      echo"<td>".$throw[$x]['date']."</td>";
+      echo"<td>".$throw[$x]['requesting_department']."</td>";
+      echo"<td>".$throw[$x]['plate_number']."</td>";
+      echo"<td>".$throw[$x]['kilometer_reading']."</td>";
+      echo"<td>".$throw[$x]['destinations']."</td>";
+      echo"</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+  }
+  @endphp
 </div>
 
 @endsection
