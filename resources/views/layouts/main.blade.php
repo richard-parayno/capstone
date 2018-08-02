de61aa7de17ae5f1ec40d28b4acaa43e6351f1ea
Richard Parayno <broodheart@gmail.com>
2018-08-02 14:42:41 +0800

    fixed favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
   <link rel="stylesheet" type="text/css" href="{{ URL::asset('/css/normalize.css') }}">
   <link rel="stylesheet" type="text/css" href="{{ URL::asset('/css/skeleton.css') }}">
   <link rel="stylesheet" type="text/css" href="{{ URL::asset('/css/style-dash.css') }}">
-  <link rel="shortcut icon" href="{{ URL::asset('/images/favicon.ico') }}">
+  <link rel="shortcut icon" href="{{ URL::asset('favicon.ico') }}">
   <link rel="stylesheet" href="https://unpkg.com/react-table@latest/react-table.css">
 
   <script>
